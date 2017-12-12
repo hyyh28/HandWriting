1e622f2edc0486194e512dc5a872c21856cbb0d1
华贇 <huayun@huayundeMacBook-Air.local>
2017-12-12 20:55:48 +0800

    hy

--- TensorflowTest.py
+++ TensorflowTest.py
@@ -15,5 +15,6 @@ for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
+print(y[1])
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
